92433351e8cb014458318977230e0deea8a6a226
wolfganghuse <wolfgang.huse@nutanix.com>
2024-02-16 16:31:05 +0100

    redeploy

--- modules/openshift/files/redeploy.sh
+++ modules/openshift/files/redeploy.sh
@@ -1,5 +1,11 @@
 rm -rf ocp.ready
 openshift-install destroy cluster
+rm -rf .openshift*
+rm -rf manifests
+rm -rf auth
+rm -rf tls
+rm -rf terraform
+rm -rf metadata.json
 cp install-config.yaml.x install-config.yaml
 sh create_ocp.sh
 touch ocp.ready
