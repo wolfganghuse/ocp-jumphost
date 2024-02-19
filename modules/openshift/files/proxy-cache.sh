e141d37464eb3b24a4cc9c102cea26d4c277691d
wolfganghuse <wolfgang.huse@nutanix.com>
2024-02-19 18:10:48 +0100

    fixed proxy-cache

--- modules/openshift/files/proxy-cache.sh
+++ modules/openshift/files/proxy-cache.sh
@@ -21,3 +21,26 @@ spec:
             - harbor.infrastructure.dachlab.net/gcr.io
           source: gcr.io
 EOF
+cat <<EOF | oc --kubeconfig=auth/kubeconfig create -f -
+apiVersion: config.openshift.io/v1
+kind: ImageDigestMirrorSet
+metadata:
+    name: mirror-ocp
+spec:
+    imageDigestMirrors:
+        - mirrors:
+            - harbor.infrastructure.dachlab.net/docker.io
+          source: docker.io
+        - mirrors:
+            - harbor.infrastructure.dachlab.net/registry.k8s.io
+          source: registry.k8s.io
+        - mirrors:
+            - harbor.infrastructure.dachlab.net/ghcr.io
+          source: ghcr.io
+        - mirrors:
+            - harbor.infrastructure.dachlab.net/quay.io
+          source: quay.io
+        - mirrors:
+            - harbor.infrastructure.dachlab.net/gcr.io
+          source: gcr.io
+EOF
