6bb5efa9911b9b11182100969977a506f0db1a6f
wolfganghuse <wolfgang.huse@nutanix.com>
2024-04-10 12:11:15 +0200

    fix registry cert chain

--- files/registry.sh
+++ files/registry.sh
@@ -2,7 +2,8 @@ mkdir certs
 mkdir docker_reg_auth
 mkdir registry
 htpasswd -bnB nutanix nutanix.1 > docker_reg_auth/htpasswd
-cp cert.* certs
+cat cert.crt ca.crt > certs/fullchain.crt
+cp cert.key certs
 
 #$CONTAINER_ENGINE login
-$CONTAINER_ENGINE container run -d -p 443:5000 --name registry -v "$(pwd)"/docker_reg_auth:/auth -v "$(pwd)"/certs:/certs -v "$(pwd)"/registry:/var/lib/registry -e REGISTRY_AUTH=htpasswd -e REGISTRY_AUTH_HTPASSWD_REALM="Registry Realm" -e REGISTRY_AUTH_HTPASSWD_PATH=/auth/htpasswd -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/cert.crt -e REGISTRY_HTTP_TLS_KEY=/certs/cert.key registry:2
+$CONTAINER_ENGINE container run -d -p 443:5000 --name registry -v "$(pwd)"/docker_reg_auth:/auth -v "$(pwd)"/certs:/certs -v "$(pwd)"/registry:/var/lib/registry -e REGISTRY_AUTH=htpasswd -e REGISTRY_AUTH_HTPASSWD_REALM="Registry Realm" -e REGISTRY_AUTH_HTPASSWD_PATH=/auth/htpasswd -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/fullchain.crt -e REGISTRY_HTTP_TLS_KEY=/certs/cert.key registry:2
